d249d3901b47fdd155946e100afc8dc160250885
Gaël <gael+git@demette.fr>
2019-04-05 16:18:09 +0200

    Move axios config into common lib

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const { join } = require('path');
 const pkg = require('./package.json');
 
 module.exports = function injectModule({ components = {} } = {}) {
+  this.requireModule('@nuxtjs/axios');
   this.addPlugin({
     src: join(__dirname, 'plugin.js'),
     options: {
       components,
       pkg,
     },
+  });
+  this.addPlugin({
+    src: join(__dirname, 'plugins/axios.js'),
   });
 };
 
