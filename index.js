040cb728c4ef0a3890532781eb22ea5023fc4a16
Gaël <gael+git@demette.fr>
2019-04-08 14:13:19 +0200

    Fix import

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const { join } = require('path');
 const pkg = require('./package.json');
 
 module.exports = function injectModule({ components = {}, api = [] } = {}) {
-  this.requireModule(['@nuxtjs/axios']);
-  this.requireModule(['@nuxtjs/pwa']);
-  this.requireModule(['@nuxtjs/toast']);
-
   this.addPlugin({
     src: join(__dirname, 'plugins/components.js'),
     options: {
@@ -23,11 +19,16 @@ module.exports = function injectModule({ components = {}, api = [] } = {}) {
   });
 
   this.addPlugin({
-    src: join(__dirname, 'api'),
+    src: join(__dirname, 'api/index.js'),
     options: {
       api,
+      pkg,
     },
   });
+
+  this.requireModule('@nuxtjs/pwa');
+  this.requireModule('@nuxtjs/toast');
+  this.requireModule('@nuxtjs/axios');
 };
 
 module.exports.meta = require('./package.json');
