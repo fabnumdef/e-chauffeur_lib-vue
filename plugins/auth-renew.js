5f561cbc48293b0b7f9d8d6bd16cec3e9f895a5c
Gaël <gael+git@demette.fr>
2019-07-08 16:16:56 +0200

    Ensure that we're client side

--- plugins/auth-renew.js
+++ plugins/auth-renew.js
@@ -1,5 +1,5 @@
 export default function ({ app: { $auth, $api, $axios } }) {
-  if (!!window && !!JSON) {
+  if (process.client && !!window && !!JSON) {
     const TOKEN_STORAGE = `${$auth.options.token.prefix}${$auth.options.defaultStrategy}`;
     const TOKEN_PREFIX = `${$auth.strategies.local.options.tokenType} `;
     const autoRenew = () => {
