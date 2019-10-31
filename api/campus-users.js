580e1815cd1462b89ebd844ef21c3722cef848cd
Robin <robin.lebert@gmail.com>
2019-10-31 19:21:18 +0100

    Add /campuses/id/users requests

---
+++ api/campus-users.js
@@ -0,0 +1,59 @@
+import { computePagination, RANGE } from './helpers';
+import { ENTITY as ENTITY_USER } from './users';
+
+const ENTITY_PLURAL = 'users';
+const ENTITY_CAMPUSES = 'campuses';
+
+export default axios => (campus, mask) => ({
+  async getUsers(offset = 0, limit = 30) {
+    const response = await axios.get(
+      `/${ENTITY_CAMPUSES}/${campus}/${ENTITY_PLURAL}`,
+      {
+        params: { campus, mask },
+        headers: {
+          [RANGE]: `${ENTITY_USER}=${offset}-${offset + limit - 1}`,
+        },
+      },
+    );
+
+    response.pagination = computePagination(response)[ENTITY_USER];
+
+    return response;
+  },
+
+  async getUser(id) {
+    const response = await axios.get(
+      `/${ENTITY_CAMPUSES}/${campus}/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
+      {
+        params: { campus, mask },
+      },
+    );
+    return response;
+  },
+
+  postUser(data) {
+    return axios.post(
+      `/${ENTITY_CAMPUSES}/${campus}/${ENTITY_PLURAL}/`,
+      data,
+      {
+        params: { campus, mask },
+      },
+    );
+  },
+
+  patchUser(id, data) {
+    return axios.patch(
+      `/${ENTITY_CAMPUSES}/${campus}/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
+      data,
+      {
+        params: { mask },
+      },
+    );
+  },
+
+  deleteUser(id) {
+    return axios.delete(
+      `/${ENTITY_CAMPUSES}/${campus}/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
+    );
+  },
+});
