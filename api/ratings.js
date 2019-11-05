375f4f48f04c32d3249a0d2187b77492202e340c
Charles Capelli <charles.capelli@qonfucius.team>
2019-11-05 20:02:39 +0100

    updated rating calls to API

--- api/ratings.js
+++ api/ratings.js
@@ -1,11 +1,27 @@
-export default axios => ({
+import { RANGE, computePagination } from './helpers';
+
+const ENTITY = 'rating';
+const ENTITY_PLURAL = 'ratings';
+
+export default axios => mask => ({
   async postRating(fields) {
     return axios.post(
-      '/ratings',
+      `/${ENTITY_PLURAL}`,
       fields,
     );
   },
-  async getRatings() {
-    return axios.get('/ratings');
+  async getRatings(offset = 0, limit = 30, search = null) {
+    const response = await axios.get(
+      `/${ENTITY_PLURAL}`,
+      {
+        params: { search, mask },
+        headers: {
+          [RANGE]: `${ENTITY}=${offset}-${offset + limit - 1}`,
+        },
+      },
+    );
+
+    response.pagination = computePagination(response)[ENTITY];
+    return response;
   },
 });
