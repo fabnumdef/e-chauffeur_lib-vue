4002449179cfbf58e84174e85f0a436e6ed97479
Gaël Demette <gael.demette+git@qonfucius.team>
2019-11-12 20:38:00 +0100

    Add campuses mock file

--- api/mock/index.js
+++ api/mock/index.js
@@ -1,6 +1,9 @@
 import MockAdapter from 'axios-mock-adapter';
+import campusesMock from './campuses';
 
 export default (axios) => {
   const mock = new MockAdapter(axios);
+  campusesMock(mock);
+
   mock.onAny().reply((config) => console.log(config));
 };
