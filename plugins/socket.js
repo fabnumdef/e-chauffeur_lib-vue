0e36b54f1883418adf0092e2c32f98c6dcb0763e
Gaël Demette <gael.demette+git@qonfucius.team>
2019-12-17 16:21:24 +0100

    load socket only client-side

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,9 @@ import io from 'socket.io-client';
 export const AUTH_HEADER_KEY = '_token.local';
 
 export default function ({ app, env, store }, inject) {
-  const ioInstance = io(app.$env ? app.$env.API_URL : env.apiUrl, { autoConnect: false });
-  Vue.use(VueSocketio, ioInstance, { store });
-  inject('io', ioInstance);
+  if (process.client) {
+    const ioInstance = io(app.$env ? app.$env.API_URL : env.apiUrl, { autoConnect: false });
+    Vue.use(VueSocketio, ioInstance, { store });
+    inject('io', ioInstance);
+  }
 }
