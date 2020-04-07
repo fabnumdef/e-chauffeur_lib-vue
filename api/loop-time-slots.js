a256485d095514df83c7ddf5e39d3d0f52d012b0
Charles <charles.capelli@qonfucius.team>
2020-04-07 19:51:40 +0200

    Loop time slots API calls

---
+++ api/loop-time-slots.js
@@ -0,0 +1,64 @@
+import merge from 'lodash.merge';
+import { computePagination, RANGE } from './helpers';
+
+export const ENTITY = 'loop-time-slot';
+export const ENTITY_PLURAL = 'loop-time-slots';
+
+export default (axios) => (mask = ',', campus = null) => {
+  const filters = {};
+  if (campus) {
+    filters.campus = campus;
+  }
+  const params = {
+    mask,
+    filters,
+  };
+  return {
+    async getTimeSlotsBetween(after, before, offset = 0, limit = 30) {
+      const response = await axios.get(
+        `/${ENTITY_PLURAL}`,
+        {
+          params: {
+            mask,
+            filters: merge({}, filters, { after, before }),
+          },
+          headers: {
+            [RANGE]: `${ENTITY}=${offset}-${offset + limit - 1}`,
+          },
+        },
+      );
+
+      response.pagination = computePagination(response)[ENTITY];
+      return response;
+    },
+
+    editTimeSlot(id, data) {
+      return axios.patch(
+        `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
+        merge(data, { campus: { id: campus } }),
+        {
+          params,
+        },
+      );
+    },
+
+    createTimeSlot(data) {
+      return axios.post(
+        `/${ENTITY_PLURAL}`,
+        merge(data, { campus: { id: campus } }),
+        {
+          params,
+        },
+      );
+    },
+
+    deleteTimeSlot(id) {
+      return axios.delete(
+        `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
+        {
+          params,
+        },
+      );
+    },
+  };
+};
