2830ed856acd978b7131002946cd7c24f10eb676
Gaël Demette <gael.demette+git@qonfucius.team>
2020-04-24 17:44:37 +0200

    Refactor users CRUD Remove global POIs CRUD

--- api/users.js
+++ api/users.js
@@ -1,5 +1,4 @@
 import merge from 'lodash.merge';
-import { computePagination, RANGE } from './abstract/helpers';
 import User from './model-query/user';
 import AbstractCRUDQuery from './abstract/crud-query';
 
@@ -26,98 +25,14 @@ export default class UsersQuery extends AbstractCRUDQuery {
       },
     }, options)));
   }
+
+  subscribeDevice(id, data, options) {
+    return this.constructor.axios.post(
+      this.constructor.getEndpoint(id, 'subscribe-device'),
+      data,
+      merge({
+        params: { mask: this.mask },
+      }, options),
+    );
+  }
 }
-
-export const deprecated = (axios) => ({
-  async getUsers(mask, {
-    offset = 0,
-    limit = 30,
-    format = null,
-    csv = {},
-  } = {}) {
-    const params = {
-      mask: csv.mask || mask,
-    };
-    const headers = {
-      [RANGE]: `${ENTITY}=${offset}-${offset + limit - 1}`,
-    };
-    if (format) {
-      headers.Accept = format;
-      params.csv = csv;
-    }
-    const response = await axios.get(
-      `/${ENTITY_PLURAL}`,
-      {
-        params,
-        headers,
-      },
-    );
-
-    response.pagination = computePagination(response)[ENTITY];
-    return response;
-  },
-
-  getUser(id, mask) {
-    return axios.get(
-      `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-      {
-        params: { mask },
-      },
-    );
-  },
-
-  patchUser(id, data, mask, { sendToken = false } = {}) {
-    return axios.patch(
-      `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-      data,
-      {
-        params: { mask },
-        headers: {
-          'X-Send-Token': sendToken,
-        },
-      },
-    );
-  },
-
-  postUser(data, mask, { sendToken = false } = {}) {
-    return axios.post(
-      `/${ENTITY_PLURAL}`,
-      data,
-      {
-        params: { mask },
-        headers: {
-          'X-Send-Token': sendToken,
-        },
-      },
-    );
-  },
-
-  postUsers(data, { sendToken = false, delimiter = ';', ignoreEmpty = true } = {}) {
-    return axios.post(
-      `/${ENTITY_PLURAL}/batch`,
-      data,
-      {
-        params: { delimiter, ignoreEmpty },
-        headers: {
-          'X-Send-Token': sendToken,
-        },
-      },
-    );
-  },
-
-  deleteUser(id) {
-    return axios.delete(
-      `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-    );
-  },
-
-  subscribeDevice(id, data, mask) {
-    return axios.post(
-      `/${ENTITY_PLURAL}/${encodeURIComponent(id)}/subscribe-device`,
-      data,
-      {
-        params: { mask },
-      },
-    );
-  },
-});
