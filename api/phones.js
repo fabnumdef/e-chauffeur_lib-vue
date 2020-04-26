19ae5e67a7ad4b3d14b155fdae96455201da1716
Gaël Demette <gael.demette+git@qonfucius.team>
2020-04-26 16:51:12 +0200

    Migrate phones

--- api/phones.js
+++ api/phones.js
@@ -1,93 +1,14 @@
-import merge from 'lodash.merge';
-import { computePagination, RANGE } from './abstract/helpers';
+import AbstractCampusCRUDQuery from './abstract/campus-crud-query';
 
 export const ENTITY = 'phone';
 export const ENTITY_PLURAL = 'phones';
 
-export default (axios) => (campus, mask) => {
-  const filters = {};
-  if (campus) {
-    filters.campus = campus.id;
+export default class PoisQuery extends AbstractCampusCRUDQuery {
+  static get ENTITY() {
+    return ENTITY;
   }
-  const params = {
-    mask,
-    filters,
-  };
-  return {
-    async getPhones({
-      offset = 0,
-      limit = 30,
-      search = null,
-      format = null,
-      csv = {},
-    } = {}) {
-      const localParams = merge(params, { search });
-      const headers = {
-        [RANGE]: `${ENTITY}=${offset}-${offset + limit - 1}`,
-      };
-      if (format) {
-        headers.Accept = format;
-        localParams.csv = csv;
-      }
-      const response = await axios.get(
-        `/${ENTITY_PLURAL}`,
-        {
-          params: localParams,
-          headers,
-        },
-      );
 
-      response.pagination = computePagination(response)[ENTITY];
-
-      return response;
-    },
-
-    async getPhone(id) {
-      return axios.get(
-        `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-        {
-          params,
-        },
-      );
-    },
-
-    postPhone(data) {
-      return axios.post(
-        `/${ENTITY_PLURAL}`,
-        merge(data, { campus }),
-        {
-          params,
-        },
-      );
-    },
-
-    postPhones(data, localParams) {
-      return axios.post(
-        `/${ENTITY_PLURAL}/batch`,
-        merge(data, { campus }),
-        {
-          params: { ...params, ...localParams },
-        },
-      );
-    },
-
-    patchPhone(id, data) {
-      return axios.patch(
-        `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-        merge(data, { campus }),
-        {
-          params,
-        },
-      );
-    },
-
-    deletePhone(id) {
-      return axios.delete(
-        `/${ENTITY_PLURAL}/${encodeURIComponent(id)}`,
-        {
-          params,
-        },
-      );
-    },
-  };
-};
+  static get ENTITY_PLURAL() {
+    return ENTITY_PLURAL;
+  }
+}
